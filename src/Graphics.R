610afb999b2fc3f280aab4586d1eda1a69229742
Eugloh <eugenie.lohmann@gmail.com>
2020-12-02 11:07:49 +0100

    Table for Barplot GGPLOT2

--- src/Graphics.R
+++ src/Graphics.R
@@ -4,10 +4,16 @@ rm(list = ls(all = TRUE))
 gc()
 ## plot 
 
-baseDir <- "~/Documents/ROSARIO/fichiers/RCA_virus"
-SourceDir <- "~/Documents/ROSARIO/fichiers/RCA_virus/src"
-OutputDir <- "~/Documents/ROSARIO/fichiers/RCA_virus/plot"
-DataDir <- "~/Documents/ROSARIO/fichiers/RCA_virus/data"
+# baseDir <- "~/Documents/ROSARIO/fichiers/RCA_virus"
+# SourceDir <- "~/Documents/ROSARIO/fichiers/RCA_virus/src"
+# OutputDir <- "~/Documents/ROSARIO/fichiers/RCA_virus/plot"
+# DataDir <- "~/Documents/ROSARIO/fichiers/RCA_virus/data"
+
+baseDir <- "/home/lohmanne/Documents/RCA_virus"
+SourceDir <- "/home/lohmanne/Documents/RCA_virus/src"
+OutputDir <- "/home/lohmanne/Documents/RCA_virus/plot"
+DataDir <- "/home/lohmanne/Documents/RCA_virus/data"
+
 
 setwd(baseDir)
 library(optparse)
@@ -67,22 +73,28 @@ dd$tax_id<-as.character(dd$tax_id)
 summary(dd)
 
 names(dd)
-# [1] "Row.names"    "Skin.5"       "Vulva.5"      "Vulva.8"      "Skin.7"       "Vulva.3"      "Vulva.7"      "Skin.10"      "Skin.3"       "Vulva.4"     
-# [11] "Skin.8"       "Vulva.9"      "Skin.12"      "Skin.1"       "Vulva.1"      "Vulva.6"      "Skin.4"       "Skin.6"       "Vulva.10"     "Vulva.11"    
-# [21] "Vulva.12"     "Skin.11"      "Skin.2"       "Vulva.2"      "Skin.9"       "tax_id"       "superkingdom" "phylum"       "class"        "order"       
-# [31] "family"       "genus"        "species"      "kingdom"      "no.rank"      "no.rank1"     "no.rank2"     "no.rank22"    "no.rank3"     "subclass"    
-# [41] "subfamily"    "subkingdom"   "suborder"     "subphylum"    "subspecies"   "subtribe"     "superfamily" 
+#  [1] "Row.names"    "RCA.Skin.5"   "RCA.Vulva.5"  "RCA.Vulva.8"  "RCA.Skin.7"  
+#  [6] "RCA.Vulva.3"  "RCA.Vulva.7"  "RCA.Skin.10"  "RCA.Skin.3"   "RCA.Vulva.4" 
+# [11] "RCA.Skin.8"   "RCA.Vulva.9"  "RCA.Skin.12"  "RCA.Skin.1"   "RCA.Vulva.1" 
+# [16] "RCA.Vulva.6"  "RCA.Skin.4"   "RCA.Skin.6"   "RCA.Vulva.10" "RCA.Vulva.11"
+# [21] "RCA.Vulva.12" "RCA.Skin.11"  "RCA.Skin.2"   "RCA.Vulva.2"  "RCA.Skin.9"  
+# [26] "SUM"          "taxid"        "ESPECES"      "tax_id"       "superkingdom"
+# [31] "phylum"       "class"        "order"        "family"       "genus"       
+# [36] "species"      "kingdom"      "no.rank"      "no.rank1"     "no.rank2"    
+# [41] "no.rank22"    "no.rank3"     "subclass"     "subfamily"    "subkingdom"  
+# [46] "suborder"     "subphylum"    "subspecies"   "subtribe"     "superfamily" 
 
-vect<-which(colnames(dd)%in%c("phylum","superkingdom", "class", "kingdom", "no.rank2", "no.rank22", "no.rank3", "subclass", "subfamily", "suborder" , "subphylum", "subspecies", "subtribe" ,"superfamily" ))
-# "phylum", "class", "kingdom", "no.rank2", "no.rank22", "no.rank3", "subclass", "subfamily", "suborder" , "subphylum", subspecies", "subtribe" ,"superfamily" 
+vect<-which(colnames(dd)%in%c("Row.names" ,"tax_id","phylum","superkingdom", "class", "kingdom", "no.rank2", "no.rank22", "no.rank3", "subclass", "subfamily", "suborder" , "subphylum", "subspecies", "subtribe" ,"superfamily" ))
+#"Row.names" ,"tax_id", "phylum", "class", "kingdom", "no.rank2", "no.rank22", "no.rank3", "subclass", "subfamily", "suborder" , "subphylum", subspecies", "subtribe" ,"superfamily" 
 Data[["Table"]]<-dd[,-vect]
 colnames(Data[["Table"]])
-# [1] "Row.names"    "RCA.Skin.5"   "RCA.Vulva.5"  "RCA.Vulva.8"  "RCA.Skin.7"   "RCA.Vulva.3"  "RCA.Vulva.7"  "RCA.Skin.10" 
-# [9] "RCA.Skin.3"   "RCA.Vulva.4"  "RCA.Skin.8"   "RCA.Vulva.9"  "RCA.Skin.12"  "RCA.Skin.1"   "RCA.Vulva.1"  "RCA.Vulva.6" 
-# [17] "RCA.Skin.4"   "RCA.Skin.6"   "RCA.Vulva.10" "RCA.Vulva.11" "RCA.Vulva.12" "RCA.Skin.11"  "RCA.Skin.2"   "RCA.Vulva.2" 
-# [25] "RCA.Skin.9"   "SUM"          "taxid"        "ESPECES"      "tax_id"       "order"        "family"       "genus"       
-# [33] "species"      "no.rank"      "no.rank1"     "subkingdom"  
-########################################################
+#  [1] "RCA.Skin.5"   "RCA.Vulva.5"  "RCA.Vulva.8"  "RCA.Skin.7"   "RCA.Vulva.3" 
+#  [6] "RCA.Vulva.7"  "RCA.Skin.10"  "RCA.Skin.3"   "RCA.Vulva.4"  "RCA.Skin.8"  
+# [11] "RCA.Vulva.9"  "RCA.Skin.12"  "RCA.Skin.1"   "RCA.Vulva.1"  "RCA.Vulva.6" 
+# [16] "RCA.Skin.4"   "RCA.Skin.6"   "RCA.Vulva.10" "RCA.Vulva.11" "RCA.Vulva.12"
+# [21] "RCA.Skin.11"  "RCA.Skin.2"   "RCA.Vulva.2"  "RCA.Skin.9"   "SUM"         
+# [26] "taxid"        "ESPECES"      "order"        "family"       "genus"       
+# [31] "species"      "no.rank"      "no.rank1"     "subkingdom" 
 
 rownames(Data[["Table"]])<- Data[["Table"]]$ESPECES
 
@@ -94,33 +106,23 @@ for (i in colnames(Data[["Table"]])){
 var_Data <- colnames(
   Data[['Table']][grepl("^RCA",
                    colnames(Data[['Table']]))])
-Data_vertical <- melt( Data[['Table']],
-                            id.vars = "ESPECES",
-                            measure.vars = var_Data)
 
-Data_vertical$Sample <- NA
 
-g <- grepl("Skin", Data_vertical$variable)
-Data_vertical$Sample[which(g)] <- "Skin"
-Data_vertical$Sample[-which(g)] <- "Vulva"
+restructuredData = melt(Data[['Table']], # table with all info
+  id = c("ESPECES","family","genus"), 
+    measure.vars = var_Data, 
+    variable_name = "number" )
 
-Data_vertical$order <- NA
-Data_vertical$family <- NA
-Data_vertical$genus <- NA
-Data_vertical$species <- NA
-Data_vertical$subkingdom <- NA
+restructuredData$from_ <- NA
+g <- grepl("Skin", restructuredData1$variable)
+restructuredData$from_[which(g)] <- "Skin"
+restructuredData$from_[-which(g)] <- "Vulva"
 
-for (el in c("order","family","genus","species","subkingdom")){
-  for (name in unique(Data_vertical$ESPECES)){
-    for (item in unique(Data_vertical$variable)){
-      Data_vertical
-    }
-    
-  }
-}
-Data[['Table']][28:36]
 
-Data_vertical$sample<- 
+## table without all the 0 values 
+reducedrestructuredData <- restructuredData[!restructuredData$value==0,]
+
+
 
 ##################
 Data[["skin"]] <- Data[["Table"]][grep("Skin|tax_id|order|family|genus|species|ESPECES",colnames(Data[["Table"]]))]
@@ -133,49 +135,8 @@ Data[["vulvareduced"]] <- Data[["vulva"]][rowSums(Data[["vulva"]]==0, na.rm=TRUE
 Data[["skinreducedsup10"]] <- Data[["skinreduced"]][which(rowSums(Data[["skinreduced"]][,0:12])>10),] # compte > 10
 Data[["vulvareducedsup10"]] <- Data[["vulvareduced"]][which(rowSums(Data[["vulvareduced"]][,0:12])>10),]
 
-tibble::as_tibble(Data[["skinreducedsup10"]])
-
+setwd(DataDir)
 write.table(Data[["skinreducedsup10"]],"skin_treshold10.csv",row.names = F,col.names = T)
 write.table(Data[["vulvareducedsup10"]],"vulva_treshold10.csv",row.names = F,col.names = T)
 # 
 
-
-
-
-# Barplots empilés avec plusieurs groupes
-plot(Data[["vulvareducedsup10"]]$ESPECES)
-barplot(vertical$variable~vertical$value)
-
-
-
-
-
-
-mensuelles <- data.frame(produit=LETTERS[1:4],
-                         ventes_2019_01=c(100,120,80,95),
-                         ventes_2019_02=c(95,135,82,95),
-                         ventes_2019_03=c(101,140,85,90),
-                         ventes_2019_04=c(100,100,70,85),
-                         ventes_2019_05=c(122,81,82,100),
-                         ventes_2019_06=c(150,50,90,100)
-)
-
-
-# étape 1 : on met la table à la verticale avec melt
-
-# on récupère les noms des colonnes "ventesXXX" à transposer
-var_ventes <- colnames(
-  mensuelles[grepl("^ventes",
-                   colnames(mensuelles))])
-mensuelles_vertical <- melt(mensuelles,
-                            id.vars = "produit",
-                            measure.vars = var_ventes)
-
-
-Data[["vulvareducedsup10"]][1:5]
-var <- colnames(Data[["vulvareducedsup10"]][1:12])
-vertical <- melt(Data[["vulvareducedsup10"]][1:13],
-                 id.vars = "ESPECES",
-                 measure.vars = var)
-
-barplot(vertical)
